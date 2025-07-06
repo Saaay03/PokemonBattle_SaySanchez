231535b52c41f938504e00b0aafddb6ae5332af9
SaySanchez <sayra.sanchez@uteca.edu.mx>
2025-07-06 15:25:37 -0600

    Initial commit

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BattleMAnager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Fighter> _fighters = new List<Fighter>();
+    [SerializeField]
+    private int _fightersNeededToStart = 2;
+    [SerializeField]
+    private UnityEvent _onBattleStarted;
+    [SerializeField]
+    private UnityEvent _onBattleEnded;
+    private Coroutine _battleCoroutine;
+    public void AddFighter(Fighter fighter)
+    {
+        _fighters.Add(fighter);
+        if (_fighters.Count >= _fightersNeededToStart)
+        {
+            StartBattle();
+        }
+    }
+    public void RemoveFighter(Fighter fighter)
+    {
+        _fighters.Remove(fighter);
+        if (_battleCoroutine != null)
+        {
+            StopCoroutine(_battleCoroutine);
+        }
+    }
+    private void InitializeFighters()
+    {
+        foreach (var fighter in _fighters)
+        {
+            fighter.InitializeFighter();
+        }
+    }
+    public void StartBattle()
+    {
+            InitializeFighters();
+            _battleCoroutine = StartCoroutine(BattleCoroutine());
+    }
+    private IEnumerator BattleCoroutine()
+    {
+        _onBattleStarted?.Invoke();
+        while (_fighters.Count > 1)
+        {
+            Fighter attacker = _fighters[Random.Range(0, _fighters.Count)];
+            Fighter defender = _fighters[Random.Range(0, _fighters.Count)];
+            while (defender == attacker)
+            {
+                defender = _fighters[Random.Range(0, _fighters.Count)];
+            }
+            attacker.transform.LookAt(defender.transform);
+            defender.transform.LookAt(attacker.transform);
+            Attack attack = attacker.AttackData.attacks[Random.Range(0, attacker.AttackData.attacks.Length)];
+            float damage = Random.Range(attack.minDamage, attack.maxDamage);
+            attacker.CharacterAnimator.Play(attack.animationName);
+            SoundManager.instance.Play(attack.soundName);
+            yield return new WaitForSeconds(attack.attackDuration);
+            defender.Health.TakeDamage(damage);
+            if (defender.Health.CurrentHealth <= 0)
+            {
+                RemoveFighter(defender);
+            }
+            yield return new WaitForSeconds(2f);
+        }
+        _onBattleEnded?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Fighter : MonoBehaviour
+{
+    [SerializeField]
+    private string _fighterName;
+    public string FighterName => _fighterName;
+    [SerializeField]
+    private AttackData _attackData;
+    public AttackData AttackData => _attackData;
+    [SerializeField]
+    private Health _health;
+    public Health Health => _health;
+    [SerializeField]
+    private Animator _characterAnimator;
+    public Animator CharacterAnimator => _characterAnimator;
+    [SerializeField]
+    private UnityEvent _onInitialize;
+    public void InitializeFighter()
+    {
+        _onInitialize?.Invoke();
+    }
+}
